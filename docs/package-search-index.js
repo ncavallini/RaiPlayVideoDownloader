df0eacd715a57c431f896f63e0c76a1daf2a60c6
Niccolò Cavallini <n.cavallini@bluewin.ch>
2022-12-11 17:42:11 +0100

    Added javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ch.ncavallini.raiplayvideodownloader"},{"l":"ch.ncavallini.raiplayvideodownloader.util"}];updateSearchResults();
