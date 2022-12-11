bb637b7866bcac254d4b5722b203371cf603ddf4
Niccolò Cavallini <n.cavallini@bluewin.ch>
2022-12-11 17:40:45 +0100

    .

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ch.ncavallini.raiplayvideodownloader"},{"l":"ch.ncavallini.raiplayvideodownloader.util"}];updateSearchResults();
